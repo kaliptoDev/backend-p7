7001f73f5491423fed1fcbebeb2e031b2287d926
kaliptoDev <121462067+kaliptoDev@users.noreply.github.com>
2023-06-16 12:33:43 +0200

    Quick clean

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const normalizePort = val => {
     }
     return false;
 };
-const port = normalizePort(process.env.PORT||'4000');
+const port = normalizePort(process.env.PORT || '4000');
 app.set('port', port);
 
 const errorHandler = error => {
@@ -42,10 +42,6 @@ const server = createServer(app);
 
 server.on('error', errorHandler);
 server.on('listening', () => {
-    // const address = server.address();
-    // const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + port;
-    // console.log('Listening on ' + bind);
-
     console.log('Serveur lancé');
 });
 
