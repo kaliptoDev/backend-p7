acc619b917a77e9720b7b1fe198333d5a3dbf2ec
kaliptoDev <121462067+kaliptoDev@users.noreply.github.com>
2023-06-17 09:41:46 +0200

    Auth basic logic implemented
    Passwords are hashed
    More security checks have to be done
    JWT has to be implemented to complete login process

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,17 +1,54 @@
+import User from '../models/User.js';
+
+import bcrypt from 'bcrypt';
+
+// const login = async (req, res) => {
+//     console.log('Requête reçue pour login!');
+//     console.log(req.headers['authorization']);
+//     res.status(200).json({
+//         message: 'Requête reçue pour login avec contenu: ' + req.headers['authorization']
+//     });
+// };
+
+// const signup = async (req, res) => {
+//     console.log("Requête reçue pour signup!");
+//     res.status(200).json({
+//         message: 'Requête reçue pour signup!'
+//     });
+// };
+
+const signup = async (req, res) => {
+    const hashedPassword = await bcrypt.hash(req.body.password, 12);
+    const user = new User({
+        email: req.body.email,
+        password: hashedPassword
+    });
+    user.save()
+        .then(() => res.status(201).json({ message: "User created successfully" }))
+        .catch(error => res.status(400).json({ error }))
+        .finally(() => console.log("User created successfully"));
+
+
+};
+
 const login = async (req, res) => {
-    console.log('Requête reçue pour login!');
-    console.log(req.headers['authorization']);
+    const foundUser = await User.findOne({ email: req.body.email });
+    if (!foundUser) {
+        return res.status(401).json({ error: 'User not found' });
+    }
+    const passwordIsValid = await bcrypt.compare(req.body.password, foundUser.password);
+    if (!passwordIsValid) {
+        return res.status(401).json({ error: 'Wrong password' });
+    }
     res.status(200).json({
-        message: 'Requête reçue pour login avec contenu: ' + req.headers['authorization']
+        userId: foundUser._id
     });
 };
 
-const signup = async (req, res) => {
-    console.log("Requête reçue pour signup!");
-    res.status(200).json({
-        message: 'Requête reçue pour signup!'
-    });
-};
+
+
+
+
 
 export {
     login,
