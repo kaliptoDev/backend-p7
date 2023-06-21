a670e62db2de27a6f798321a6ecda51ec098dcdb
kaliptoDev <121462067+kaliptoDev@users.noreply.github.com>
2023-06-21 14:45:10 +0200

    Edited books.js getBookByID fixed getBestBooks created createBook fixed Some comments for debug removed

--- controllers/books.js
+++ controllers/books.js
@@ -6,54 +6,48 @@ import { validateToken } from "./auth.js";
 const getBooks = async (req, res) => {
     const books = await Book.find();
     books == null ? res.status(404).json({ message: "No books found" }) : res.status(200).json(books);
-    console.log(books)      //! Debug
 };
 
 const getBookByID = async (req, res) => {
-    const book = await Book.findById(req.body.id);
+    const book = await Book.findById(req.params.id);
     book == null ? res.status(404).json({ message: "No book found" }) : res.status(200).json(book);
-    console.log(book)   //! Debug
 };
 
-const getBestBooks = (req, res) => {
-
+const getBestBooks = async (req, res) => { 
+    const books = await Book.find().sort({ averageRating: -1 }).limit(3);
+    books == null ? res.status(404).json({ message: "No books found" }) : res.status(200).json(books);
 };
 
-const createBook =  (req, res) => {
-try {
-    const book = JSON.parse(req.body.book);
+const createBook = (req, res) => {
+    try {
+        const book = JSON.parse(req.body.book);
 
-    book._id? delete book._id : null;
-    book._userId? delete book._userId : null;
+        book.userId ? delete book.userId : null;
 
-    const newBook = new Book({
-        ...book,
-        imageUrl: `${req.protocol}://${req.get('host')}/book_covers/${req.file.filename}`,
-        ratings: [],
-        averageRating: 0
-    });
+        book.userId = req.userId;
 
-    newBook.save()
-        .then(() => res.status(201).json({ message: "Book created successfully" }))
-        .catch(error => res.status(400).json({ error }))
-        .finally(() => console.log("Book created successfully"));  //! Debug
+        const newBook = new Book({
+            ...book,
+            imageUrl: `${req.protocol}://${req.get('host')}/book_covers/${req.file.filename}`,
+            ratings: [],
+            averageRating: 0
+        });
+
+        newBook.save()
+            .then(() => res.status(201).json({ message: "Book created successfully" }))
+            .catch(error => res.status(400).json({ error }))
+            .finally(() => console.log("Book created successfully"));  //! Debug
     } catch (error) {
         console.log(error)   //! Debug
         res.status(400).json({ error })
     }
 };
 
-// const createBook = (req, res) => {
-//     console.log(req)   //! Debug
-//     console.log(req.body.book.title)   //! Debug
-//     const book = req.body.book;
-//     const parsedBook = JSON.parse(book);
-//     console.log(parsedBook.userId)   //! Debug
-// };
-
 const updateBook = (req, res) => { };
 
-const deleteBook = (req, res) => { };
+const deleteBook = (req, res) => {
+
+};
 
 const rateBook = (req, res) => { };
 
