226049c59f5bc2d65bab4e82483c347fd80bd4a1
kaliptoDev <121462067+kaliptoDev@users.noreply.github.com>
2023-06-22 14:59:26 +0200

    Edited utils.js
    Added both genToken and validateToken functions

--- utils/utils.js
+++ utils/utils.js
@@ -1,4 +1,5 @@
 import fs from 'fs';
+import jwt from 'jsonwebtoken';
 
 const deleteFile = (path) => {
     fs.unlink(`./${path}`, (err) => {
@@ -9,4 +10,40 @@ const deleteFile = (path) => {
     });
 };
 
-export default deleteFile;
+const genToken = (userId, key) => {
+    return jwt.sign({ userId }, key, {
+        algorithm: 'HS256',
+        expiresIn: process.env.TOKEN_EXPIRATION
+    });
+};
+
+const validateToken = (req, res, next) => {
+    try {
+        const token = req.headers.authorization.split(' ')[1];
+        if (!token) {
+            console.log('No token provided, please connect')   //! Debug
+            return res.status(401).json({ error: 'No token provided, please reconnect' });
+        }
+
+        const decodedToken = jwt.verify(token, process.env.TOKEN_KEY);
+        const userId = decodedToken.userId;
+
+        jwt.verify(token, process.env.TOKEN_KEY, (err, decoded) => {
+            if (err) {
+                console.log('Invalid token, please reconnect')   //! Debug
+                return res.status(401).json({ error: 'Invalid token, please reconnect' });
+            }
+        });
+        req.userId? delete req.userId : null
+        req.userId = userId;
+
+        next();
+    } catch (error) {
+        console.log(error)  //! Debug
+        return res.status(401).json({ error: 'Invalid token, please reconnect' });
+    }
+
+}
+
+
+export { deleteFile, genToken, validateToken };
