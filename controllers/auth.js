416d1945b202fa86367672c875f74d3cd67aa475
kaliptoDev <121462067+kaliptoDev@users.noreply.github.com>
2023-06-23 16:10:15 +0200

    Made controllers simple and intuitive

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,57 +1,15 @@
-import User from '../models/User.js';
+import { Router } from 'express';
+import {
 
-import bcrypt from 'bcrypt';
-import { genToken } from '../utils/utils.js';
-
-const signup = async (req, res) => {
-    try {
-        if (!req.body.email || !req.body.password) {
-            return res.status(400).json({ error: 'Both email and password fields must be filled' });
-        }
-        req.body.email = req.body.email.toLowerCase();
-        const foundUser = await User.findOne({ email: req.body.email });
-        if (foundUser) {
-            console.log('Email already used by another user')   //! Debug
-            return res.status(401).json({ error: 'Email already used by another user' });
-        }
-        console.log(req.body.email, req.body.password)    //! Debug
-        const hashedPassword = await bcrypt.hash(req.body.password, 12);
-        const user = new User({
-            email: req.body.email,
-            password: hashedPassword
-        });
-        user.save()
-            .then(() => res.status(201).json({ message: "User created successfully" }))
-            .then(() => console.log("User created successfully"))  //! Debug
-            .catch(error => res.status(400).json({ error }));
-    } catch (error) {
-        console.log(error)   //! Debug
-        res.status(400).json({ error })
-    }
-};
-
-const login = async (req, res) => {
-    try {
-        const foundUser = await User.findOne({ email: req.body.email.toLowerCase() });
-        if (!foundUser) {
-            return res.status(401).json({ error: 'User not found' });
-        }
-        const passwordIsValid = await bcrypt.compare(req.body.password, foundUser.password);
-        if (!passwordIsValid) {
-            return res.status(401).json({ error: 'Wrong password' });
-        }
-        res.status(200).json({
-            userId: foundUser._id,
-            token: genToken(foundUser.id, process.env.TOKEN_KEY)
-        });
-    }
-    catch (error) {
-        console.log(error)   //! Debug
-        res.status(400).json({ error })
-    }
-};
-
-export {
     login,
     signup
-}
+
+} from '../services/auth.js';
+
+const router = Router();
+
+router.post('/signup', signup)
+
+router.post('/login', login)
+
+export default router;
--- controllers/books.js
+++ controllers/books.js
@@ -1,110 +1,34 @@
-import Book from "../models/Book.js";
-import { deleteFile } from "../utils/utils.js";
+import { Router } from 'express';
+import {
+    getBooks,
+    getBookByID,
+    getBestBooks,
+    createBook,
+    updateBook,
+    deleteBook,
+    rateBook
+} from '../services/books.js';
 
+import { validateToken } from '../middlewares/jwt.js';
 
-const getBooks = async (req, res) => {
-    const books = await Book.find();
-    books == null ? res.status(404).json({ message: "No books found" }) : res.status(200).json(books);
-};
+import multer from '../middlewares/multer-config.js';
 
-const getBookByID = async (req, res) => {
-    const book = await Book.findById(req.params.id);
-    book == null ? res.status(404).json({ message: "No book found" }) : res.status(200).json(book);
-};
+import { optimizeImage } from '../utils/utils.js';
 
-const getBestBooks = async (req, res) => {
-    const books = await Book.find().sort({ averageRating: -1 }).limit(3);
-    books == null ? res.status(404).json({ message: "No books found" }) : res.status(200).json(books);
-};
+const router = Router();
 
-const createBook = (req, res) => {
-    try {
-        const book = JSON.parse(req.body.book);
-        book.userId ? delete book.userId : null;
-        book.userId = req.userId;
-        const newBook = new Book({
-            ...book,
-            imageUrl: `${req.protocol}://${req.get('host')}/book_covers/${req.file.filename}`,
-            ratings: [],
-            averageRating: 0
-        });
-        newBook.save()
-            .then(() => res.status(201).json({ message: "Book created successfully" }))
-            .then(() => console.log("Book created successfully"))   //! Debug
-            .catch(error => res.status(400).json({ error }))
-    } catch (error) {
-        console.log(error)   //! Debug
-        res.status(400).json({ error })
-    }
-};
+router.get('/', getBooks)
 
-const updateBook = async (req, res) => {
+router.get('/bestrating', getBestBooks)
 
-    try {
-        const book = JSON.parse(req.body.book);
-        const oldBook = await Book.findById(req.params.id);
-        if (req.userId == oldBook.userId) {
-            try {
-                const oldBook = await Book.findById(req.params.id);
-                const oldUrl = oldBook.imageUrl;
-                Book.updateOne({ _id: req.params.id }, {
-                    ...book,
-                    imageUrl: `${req.protocol}://${req.get('host')}/book_covers/${req.file ? req.file.filename : oldUrl.split('/book_covers/')[1]}`
-                })
-                    .then(() => res.status(200).json({ message: "Book updated successfully" }))
-                    .catch(error => res.status(400).json({ error }))
-                deleteFile(oldUrl.split(`${req.protocol}://${req.get('host')}`)[1]);
-            } catch (error) {
-                res.status(400).json({ error })
-            };
-        } else {
-            res.status(403).json({ error: "You are not allowed to do this" })
-        }
-    }
-    catch (error) {
-        res.status(400).json({ error })
-    }
-};
+router.get('/:id', getBookByID)
 
-const deleteBook = async (req, res) => {
-    try {
-        const book = await Book.findById(req.params.id);
-        if (req.userId == book.userId) {
-            Book.findById(req.params.id)
-                .then(book => {
-                    const filename = book.imageUrl.split('/book_covers/')[1];
-                    deleteFile(`book_covers/${filename}`);
-                })
-                .catch(error => res.status(500).json({ error }));
-            Book.deleteOne({ _id: req.params.id })
-                .then(() => res.status(200).json({ message: "Book deleted successfully" }))
-                .catch(error => res.status(400).json({ error }))
-        } else {
-            res.status(403).json({ error: "You are not allowed to do this" })
-        }
-    }
-    catch (error) {
-        res.status(400).json({ error })
-    }
-};
+router.post('/', validateToken, multer, optimizeImage, createBook)
 
-const rateBook = async (req, res) => {
-    try {
-        const book = await Book.findById(req.params.id);
-        Book.updateOne({ _id: req.params.id }, {
-            ratings: [...book.ratings, { userId: req.userId, grade: req.body.rating }]
-        })
-            .then(() => console.log("Book rated successfully"))
-            .catch(error => res.status(400).json({ error }))
-        let sum = req.body.rating;
-        book.ratings?.length > 0 ? book.ratings.map(rating => sum = sum + rating.grade) : book.averageRating = sum;
-        book.averageRating = sum / (book.ratings?.length + 1);
-        book.save()
-        res.status(200).json(book)
-    }
-    catch (error) {
-        res.status(400).json({ error })
-    }
-};
+router.put('/:id', validateToken, multer, optimizeImage, updateBook)
 
-export { getBooks, getBookByID, getBestBooks, createBook, updateBook, deleteBook, rateBook };
+router.delete('/:id', validateToken, deleteBook)
+
+router.post('/:id/rating', validateToken, rateBook)
+
+export default router;
